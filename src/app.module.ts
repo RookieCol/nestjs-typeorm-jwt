0e1ae550d32483dfff1942211942a4fffa8ec0f0
rookie <juan.diegoam@hotmail.com>
2023-11-14 11:46:14 -0500

    :bricks: DatabaseModule and Docker config
    DatabaseModule included and Docker modified for development

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './config/modules/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
-    })
+      envFilePath: '.env.develop',
+      isGlobal: true
+    }),
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,8 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('combined'));
-
   const configService = app.get(ConfigService);
-  console.log(configService.get('POSTGRES_PORT'));
-  
-  await app.listen(3000);
+  const port = configService.get('PORT');
+  await app.listen(port);
 }
 bootstrap();
